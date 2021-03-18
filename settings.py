cc6fbe95aece517f664176e39bfbe3ed44292033
markrmoravek <markrmoravek@gmail.com>
2021-03-18 16:51:02 -0700

    Updated graphics, added projectiles and music

--- settings.py
+++ settings.py
@@ -8,7 +8,13 @@ class Settings:
 		# Screen settings
 		self.screen_width = 1200
 		self.screen_height = 800
-		self.bg_color = (randint(0,255), randint(0,255), randint(0,255))
+		self.bg_color = (0,0,0)
 
 		#Ship settings
 		self.ship_speed = 1.5
+
+		#Bullet settings
+		self.bullet_speed = 1.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (0,128,128)
